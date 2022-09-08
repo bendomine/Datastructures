bfc506d1d31cedcd8e1313afca1e68d5ea831278
Ben Domine <bdomine2024@kehillahstudent.org>
2022-09-08 14:48:35 -0700

    Nope

---
+++ Binary Search Tree/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+	Node parent;
+	Node leftChild;
+	Node rightChild;
+	int value;
+	
+	Node(){
+		parent = null;
+		leftChild = null;
+		rightChild = null;
+		value = 0;
+	}
+
+}
