f9476c3a1c42e850b9a7f1c2bafd104314ec4b3d
Ben Domine <bdomine2024@kehillahstudent.org>
2022-12-12 20:58:19 -0800

    Committed modifications

--- Fourier/Complex.java
+++ Fourier/Complex.java
@@ -1,5 +1,5 @@
 public class Complex{
-	private double _this; // Mr. Kuszmaul told me to
+	private double _this; // Mr. Kuszmaul told me to; actually the real component
 	private double imaginary;
 
 	public Complex(double _real, double _imaginary){
@@ -8,17 +8,25 @@ public class Complex{
 	}
 
 	public Complex add(Complex o){
-		return new Complex(this._this + o.get_this(), imaginary+o.getImaginary());
+		return new Complex(this._this + o.getReal(), imaginary+o.getImaginary());
 	}
 
 	public Complex multiply(Complex o){
 		// (a + bi) (c + di) = ac-bd + (ad + bc)i
-		return new Complex(this._this*o.getImaginary() - imaginary*o.get_this(), this._this*o.getImaginary() + imaginary*o.get_this());
+		return new Complex(this._this*o.getReal() - imaginary*o.getImaginary(), this._this*o.getImaginary() + imaginary*o.getReal());
+	}
+
+	public Complex subtract(Complex o){
+		return new Complex(this._this - o.getReal(), imaginary - o.getImaginary());
+	}
+
+	public Complex divide(Complex o){
+		return multiply(new Complex(1.0/o.getReal(), 1.0/o.getImaginary()));
 	}
 
 	
 
-	public double get_this() {
+	public double getReal() {
 		return this._this;
 	}
 	
